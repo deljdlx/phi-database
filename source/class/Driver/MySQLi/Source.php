50cca46dcfbf4e148968bbb380e5994f9285b6c3
Julien Delsescaux <jdelsescaux@skillbase.fr>
2019-03-06 12:52:00 +0100

    synchro

--- source/class/Driver/MySQLi/Source.php
+++ source/class/Driver/MySQLi/Source.php
@@ -85,6 +85,10 @@ class Source extends \MySQLi implements Driver
         $bindParameters = array_merge($bindParameters, $values);
 
 
+        echo '<pre id="' . __FILE__ . '-' . __LINE__ . '" style="border: solid 1px rgb(255,0,0); background-color:rgb(255,255,255)">';
+        echo '<div style="background-color:rgba(100,100,100,1); color: rgba(255,255,255,1)">' . __FILE__ . '@' . __LINE__ . '</div>';
+        print_r($query);
+        echo '</pre>';
 
         call_user_func_array(
             array($statement, 'bind_param'), $bindParameters
